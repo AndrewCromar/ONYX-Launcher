7c7a5b2aee306ca60860f0da6929345b5c787865
Andrew Cromar <andrewmcromar@gmail.com>
2024-01-17 21:10:05 -0800

    Create app.py

---
+++ Source Code/Python/app.py
@@ -0,0 +1,9 @@
+from customtkinter import *
+
+app = CTk()
+app.geometry("500x400")
+
+btn = CTkButton(master=app, text="Click Me", corner_radius=16)
+btn.place(relx=0.5, rely=0.5, anchor="center")
+
+app.mainloop()
