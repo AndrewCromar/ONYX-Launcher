d72f3d0552da043d63539a3fac0d7e3725caf11c
Andrew Cromar <andrewmcromar@gmail.com>
2024-01-17 21:27:42 -0800

    Update app.py

--- Source Code/Python/app.py
+++ Source Code/Python/app.py
@@ -3,7 +3,17 @@ from customtkinter import *
 app = CTk()
 app.geometry("500x400")
 
-btn = CTkButton(master=app, text="Click Me", corner_radius=16)
+set_appearance_mode("dark")
+
+tabView = CTkTabview(master=app, height=390, width=490)
+tabView.pack()
+
+tabView.add("Games")
+tabView.add("News")
+tabView.add("Settings")
+tabView.add("Account")
+
+btn = CTkButton(master=tabView.tab("Games"), text="Click Me", corner_radius=16)
 btn.place(relx=0.5, rely=0.5, anchor="center")
 
 app.mainloop()
