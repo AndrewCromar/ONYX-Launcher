3ce1e257ac62bed2a5fd32410fc9dc2aee4fbe54
Andrew Cromar <andrewmcromar@gmail.com>
2024-01-18 17:58:24 -0800

    stuff

--- Source Code/Python/app.py
+++ Source Code/Python/app.py
@@ -7,6 +7,7 @@ print("launching launcher")
 def AddTab(form, title): form.add(title)
 def Discord(): webbrowser.open("https://discord.com/invite/2maTr7RQQQ", new=0, autoraise=True)
 def Github(): webbrowser.open("https://github.com/AndrewCromar/ONYX-Launcher", new=0, autoraise=True)
+def Youtube(): webbrowser.open("https://www.youtube.com/@ONYXDevelopment", new=0, autoraise=True)
 
 app = CTk()
 app.geometry("1000x800")
@@ -18,6 +19,7 @@ hh_logo = Image.open("img/hell_hotel_logo.png")
 rd_logo = Image.open("img/ragdoll_destroyers_logo.png")
 d_logo = Image.open("img/discord_logo.png")
 gh_logo = Image.open("img/github_logo.png")
+yt_logo = Image.open("img/youtube_logo.png")
 
 title = CTkLabel(app, text="ONYX Game Launcher", font=(CTkFont, 20)).place(relx=0.5, rely=0.05, anchor="center")
 
@@ -36,7 +38,8 @@ AddTab(games_tabs, "Ragdoll Destroyers")
 hh_image = CTkLabel(games_tabs.tab("Hell Hotel"), image=CTkImage(dark_image=hh_logo, light_image=hh_logo, size=(250, 250)), text="").place(relx=0, rely=0)
 hh_image = CTkLabel(games_tabs.tab("Ragdoll Destroyers"), image=CTkImage(dark_image=rd_logo, light_image=rd_logo, size=(250, 250)), text="").place(relx=0, rely=0)
 
-btn_discord = CTkButton(master=master_tabs.tab("Settings"), command=Discord, text="Join Discord", image=CTkImage(dark_image=d_logo, light_image=d_logo, size=(31, 24)), fg_color="#000000", border_color="#7289da", text_color="#7289da", border_width=3, corner_radius=20, font=(CTkFont, 15)).place(relx=0.025, rely=0)
+btn_discord = CTkButton(master=master_tabs.tab("Settings"), command=Discord, text="Join Discord", image=CTkImage(dark_image=d_logo, light_image=d_logo, size=(31, 24)), fg_color="#313338", border_color="#7289da", text_color="#7289da", border_width=3, corner_radius=20, font=(CTkFont, 15)).place(relx=0.025, rely=0)
 btn_github = CTkButton(master=master_tabs.tab("Settings"), command=Github, text="GitHub", image=CTkImage(dark_image=gh_logo, light_image=gh_logo, size=(24, 24)), fg_color="#aaaaaa", text_color="#000000", border_color="#000000", border_width=4, corner_radius=20, font=(CTkFont, 15)).place(relx=0.025, rely=0.075)
+btn_youtube = CTkButton(master=master_tabs.tab("Settings"), command=Youtube, text="Youtube", image=CTkImage(dark_image=yt_logo, light_image=yt_logo, size=(35, 24)), fg_color="#ffffff", text_color="#ff0000", border_color="#ff0000", border_width=4, corner_radius=20, font=(CTkFont, 15)).place(relx=0.025, rely=0.15)
 
 app.mainloop()
